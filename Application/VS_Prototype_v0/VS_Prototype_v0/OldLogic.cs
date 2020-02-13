1aeb18cdec604727fa5834615492210babf0f9e6
rfid19230 <58638987+rfid19230@users.noreply.github.com>
2020-02-13 22:26:18 +0300

    Add files via upload

---
+++ Application/VS_Prototype_v0/VS_Prototype_v0/OldLogic.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace VS_Prototype_v0
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Действия, выполняемые при нажатии кнопки "Подключение"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            // Выбор номера используемого порта
+            serialPort.PortName = chosenPortComboBox.Text;
+            // Открытие порта с обработкой исключений, возникающих при невозможности открытия
+            try
+            {
+                serialPort.Open();
+            }
+            catch (Exception exception)
+            {
+                string info = "Подключение к порту " + serialPort.PortName + ": Не удалось\r\n";
+                toDebugTextBox(info);
+            }
+            // При успешном открытии 
+            if (serialPort.IsOpen)
+            {
+                // Отправка команды подключния на Arduino
+                serialPort.Write(COMMAND_DISCONNECT);
+                serialPort.Write(COMMAND_CONNECT);
+                // Вывод информации о работе программы
+                string info = "Подключение к порту " + serialPort.PortName + ": Успешно\r\n";
+                toDebugTextBox(info);
+                connectionStateLabel.BackColor = Color.Lime;
+                connectionStateLabel.Text = "Подключено";
+                connectButton.Enabled = false;
+                disconnectButton.Enabled = true;
+                chosenPortComboBox.Enabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Действия, выполняемые при нажатии кнопки "Отключение"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            // Отправка на Arduino команды разрыва соединения
+            serialPort.Write(COMMAND_DISCONNECT);
+            // Закрытие порта
+            try
+            {
+                serialPort.Close();
+            }
+            catch (Exception exception)
+            {
+
+            }
+            // Вывод информации о работе программы
+            string info = "Отключение порта " + serialPort.PortName + ": Успешно\r\n";
+            toDebugTextBox(info);
+            connectionStateLabel.BackColor = System.Drawing.Color.Red;
+            connectionStateLabel.Text = "Отключено";
+            connectButton.Enabled = true;
+            disconnectButton.Enabled = false;
+            chosenPortComboBox.Enabled = true;
+
+            currentUid = "";
+        }
+
+        /// <summary>
+        /// Действия, выполняемые при получении данных от Arduino. 
+        /// Основная функция программы.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void serialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            // Считывание размера uid прочитанной метки
+            int uidSize = Convert.ToInt32(serialPort.ReadLine());
+            // Считывание uid метки
+            string uid = "";
+            for (int i = 0; i < uidSize; ++i)
+            {
+                // Обработка очередного байта uid
+                string rawUidByte = serialPort.ReadLine();
+                string uidByte = rawUidByte.Substring(0, rawUidByte.Length - 1);
+                if (uidByte.Length == 1)
+                {
+                    uidByte = "0" + uidByte;
+                }
+                uid += uidByte;
+            }
+
+            // Вывод информации о работе программы
+            string uidInfo = "Считана RFID-метка: uidSize = " + uidSize.ToString() + ", uid = " + uid + "\r\n";
+            toDebugTextBox(uidInfo);
+
+            // Анализ прочитанной метки и выбор дальнейших действий
+            if (currentUid == "")
+            {
+                // Считанный uid рассматривается как пропуск
+                string passInfo = "Рассматривается как пропуск\r\n";
+                toDebugTextBox(passInfo);
+
+                // Обращение в БД для проверки прав доступа
+                if (isValidPass(uid))
+                {
+                    // Если доступ разрешен - отправка команды на открытие замка, запоминание текущего id пропуска
+                    currentUid = uid;
+                    serialPort.Write(COMMAND_VALID_PASS);
+                    // Вывод информации о работе программы
+                    string info = "Выполнен запрос в БД. Доступ разрешен, замок открыт\r\n";
+                    toDebugTextBox(info);
+                }
+                else
+                {
+                    // Если доступ запрещен
+                    serialPort.Write(COMMAND_INVALID_PASS);
+                    // Вывод информации о работе программы
+                    string info = "Выполнен запрос в БД. Доступ не разрешен, замок не открыт\r\n";
+                    toDebugTextBox(info);
+                }
+            }
+            else
+            {
+                // Идет процесс взятия и возврата вещей
+                // Проверка на завершение процесса взятия и возврата вещей
+                if (currentUid == uid)
+                {
+                    // Повторное прикладывание пропуска - признак завершения процесса взятия вещей
+                    // Сброс текущего uid
+                    currentUid = "";
+                    // Отправка команды завершения сеанса взятия и возврата вещей
+                    serialPort.Write(COMMAND_END_SESSION);
+                    // Вывод информации о работе программы
+                    string info = "Повторное прикладывание пропуска. Конец взятия и возврата вещей\r\n";
+                    toDebugTextBox(info);
+                }
+                else
+                {
+                    // Считанный uid рассматривается как ценность
+                    string valueInfo = "Рассматривается как ценность\r\n";
+                    toDebugTextBox(valueInfo);
+
+                    // Проверка ценности
+                    if (isValidValue(uid))
+                    {
+                        // Информация о ценности есть в БД
+                        string validValueInfo = "Выполнен запрос в БД. Ценность найдена в списке\r\n";
+                        toDebugTextBox(validValueInfo);
+
+                        // Проверка, взяте или возврат
+                        string action = actionWithValue(currentUid, uid);
+                        //
+                        if (action == "taking")
+                        {
+                            // Если возврат, отправка соответствующей команды на Arduino
+                            serialPort.Write(COMMAND_VALUE_TAKEN);
+                            // Вывод информации о работе программы
+                            string info = "Выполнен запрос в БД. Действие - взятие\r\n";
+                            toDebugTextBox(info);
+                            // Внесение изменений в БД
+                            changeTakenValues(currentUid, uid, action);
+                            // Отображение изменений
+                            if (autoRefreshTakenValuesInfoCheckBox.Checked)
+                            {
+                                selectFromTable("takenvalues_info", getTakenValuesInfoFields());
+                            }
+                        }
+                        else
+                        {
+                            if (action == "returning")
+                            {
+                                // Если озврат, отправка соответствующей команды на Arduino
+                                serialPort.Write(COMMAND_VALUE_RETURNED);
+                                // Вывод информации о работе программы
+                                string info = "Выполнен запрос в БД. Действие - возврат\r\n";
+                                toDebugTextBox(info);
+                                // Внесение изменений в БД
+                                changeTakenValues(currentUid, uid, action);
+                                // Отображение изменений
+                                if (autoRefreshTakenValuesInfoCheckBox.Checked)
+                                {
+                                    Invoke(new Action(() => selectFromTable("takenvalues_info", getTakenValuesInfoFields())));
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // Информация о ценности отсутствует в БД
+                        serialPort.Write(COMMAND_INVALID_VALUE);
+                        // Вывод информации о работе программы
+                        string info = "Выполнен запрос в БД. Ценность не найдена в списке\r\n";
+                        toDebugTextBox(info);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Application/VS_Prototype_v0/VS_Prototype_v0/Server.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace VS_Prototype_v0
+{
+    public partial class MainForm : Form
+    {
+        // Порт и ip-адрес сервера
+        const int srv_port = 19230;
+        const string srv_ip = "172.18.160.26";
+
+        public TcpListener srv = null;
+        public static bool srv_stop;
+
+        /// <summary>
+        /// Действия, выполняемые при нажатии на кнопку "Запустить сервер"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void startServerButton_Click(object sender, EventArgs e)
+        {
+            // Создание нового потока, играющего роль сервера
+            Thread s = new Thread(() =>
+            {
+                // Описание действий, выполняемых в серверном потоке
+                // Настройка некоторых элементов интерфейса
+                Invoke(new Action(() =>
+                {
+                    startServerButton.Enabled = false;
+                    stopServerButton.Enabled = true;
+                    serverStateLabel.BackColor = Color.Lime;
+                }));
+                toDebugTextBox("Сервер запущен\r\n");
+                // Установка флага состояния сервера
+                srv_stop = false;
+                try
+                {
+                    // Запуск сервера
+                    srv = new TcpListener(IPAddress.Parse(srv_ip), srv_port);
+                    srv.Start();
+
+                    // Цикл ожидания подключений с проверкой флага состояния сервера
+                    while (!srv_stop)
+                    {
+                        // Непосредственно ожидание подключения
+                        while (!srv_stop && !srv.Pending())
+                        {
+                            Thread.Sleep(100);
+                        }
+                        if (!srv_stop)
+                        {
+                            // При входящем подключении
+                            //                            Thread c = new Thread(() =>
+                            //                            {
+                            NetworkStream stream = null;
+                            TcpClient client = null;
+                            try
+                            {
+                                // Установка соединения с клиентом
+                                client = srv.AcceptTcpClient();
+                                Thread.Sleep(100);
+                                toDebugTextBox("Зафиксировано входящее подключение\r\n");
+
+                                // Считывание полученного сообщения
+                                stream = client.GetStream();
+                                byte[] data = new byte[64]; // буфер для получаемых данных
+
+                                // получаем uid
+                                StringBuilder builder = new StringBuilder();
+                                int bytes = 0;
+                                do
+                                {
+                                    bytes = stream.Read(data, 0, data.Length);
+                                    builder.Append(Encoding.ASCII.GetString(data, 0, bytes));
+                                }
+                                while (stream.DataAvailable);
+
+                                // Обработка полученного uid
+                                string uid = builder.ToString();
+                                toDebugTextBox("Получен считанный uid = " + uid + "\r\n");
+                                if (uid != "00000000")
+                                {
+                                    string command = onUidReceived(uid);
+                                    toDebugTextBox("Код сгенерированной команды = " + command + "\r\n");
+
+                                    // отправляем обратно команду
+                                    data = Encoding.ASCII.GetBytes(command);
+                                    stream.Write(data, 0, data.Length);
+                                }
+                                else
+                                {
+                                    toDebugTextBox("Получено сообщение проверки соединения\r\n");
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                // При ошибке
+                                toDebugTextBox(ex.Message + "\r\nОшибка в клиентском потоке\r\n");
+                            }
+                            finally
+                            {
+                                // При завершении работы
+                                if (stream != null)
+                                    stream.Close();
+                                if (client != null)
+                                {
+                                    client.Close();
+                                    toDebugTextBox("Завершено обслуживание входящего подключения\r\n");
+                                }
+                            }
+                            //                            });
+                            //                            c.IsBackground = true;
+                            //                            c.Start();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // При ошибке
+                    toDebugTextBox(ex.Message + "\r\nОшибка сервера\r\n");
+                }
+                finally
+                {
+                    // При ошибке завершения работы
+                    if (srv != null)
+                    {
+                        srv.Stop();
+                    }
+                    Invoke(new Action(() =>
+                    {
+                        startServerButton.Enabled = true;
+                        stopServerButton.Enabled = false;
+                        serverStateLabel.BackColor = Color.Red;
+                    }));
+                    toDebugTextBox("Сервер остановлен\r\n");
+                }
+            });
+            s.IsBackground = true;
+            s.Start();
+        }
+
+        /// <summary>
+        /// Действия, выполняемые при нажатии на кнопку "Остановить сервер"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void stopServerButton_Click(object sender, EventArgs e)
+        {
+            // Установка флага остановки сервера
+            srv_stop = true;
+        }
+    }
+}
